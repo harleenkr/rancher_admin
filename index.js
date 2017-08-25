f508813407e42bb496cd35066a41c2596c74d226
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-25 14:36:31 +0530

    type(feat): added endpoint rancher that displays data on client side

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const reThinkPlus = require('rethink-plus')
-const moment = require('moment')
-var superagent = require('superagent')
-const dbData = new reThinkPlus({
-  })
-async function getRancher(){
-try{
-  var responseDetails = await superagent.get('https://rancher.rudimk.net/v2-beta/projects/1a5/containers/').auth('E4E078C4379953D3B913','663rrpxj98SjBm4VfkzGenxTZLyknYiLTzXn3rzD');
-  var responseRancher = responseDetails.body.data;
-  var rancherDataWithTime = {timestamp: moment().format('YYYY-MM-DD HH:mm:SS'), responseRancher: responseRancher}
-  var insertedRancher = await dbData.db('Ops').table('Containers').insert(rancherDataWithTime).run()
-  return insertedRancher;
-}
- catch(e){
-   console.log(e)
- }
-}
-getRancher().then(function(rancherData){
-    //console.log(`The actual Rancher Data's ID is ${rancherData['generated_keys'][0]}.`);
-})
+var express = require('express');
+var app = express();
+const dataDetails = require('./dataDetails')
+var router = express.Router()
+app.use('/',router);
+router.get('/rancher',dataDetails.ranchers);
+router.get('/testdata', function(req,res){
+res.send("check initial Test Data details");
+});
+//other invalid routes here
+router.get('*',function(req,res){
+res.send("Sorry, this is an invalid Url");
+});
+app.listen(3020,function(req,res){
+console.log("Server is running at port:3020")
+});
