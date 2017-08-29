bb29e7da067c15feac057bce31b6059d9b197b87
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-29 12:26:53 +0530

    type(feat): email and PUBSUB with redis

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ var app = express();
 const dataDetails = require('./dataDetails')
 // import the saveDataInRedis.js
 const saveDataInRedis = require('./saveDataInRedis')
+
 // get an instance of the express Route
 var router = express.Router()
 // all of our routes will be prefixed with /
 app.use('/', router);
-
 //This endPoint is used to display the rancher data on client side
 router.get('/rancher', dataDetails.ranchers);
 
 //This endPoint is used to save the data in Redis
 router.get('/datasavedInRedis', function (req, res) {
-  saveDataInRedis.setClient;
-  saveDataInRedis.getClient;
+  saveDataInRedis.publisher;
   res.send("save Data details");
 });
 
 //other invalid routes here
 router.get('*', function (req, res) {
+  //console.log('enter correct Url again');
   res.send("Sorry, this is an invalid Url");
 });
 // listens the application
--- saveDataInRedis.js
+++ saveDataInRedis.js
@@ -1,23 +1,37 @@
 //require REDIS
 var redis = require('redis');
-//creates a new client
-var client = redis.createClient();
+subscriber = redis.createClient()
+publisher = redis.createClient();
 
-//creates a sets the key-value inside REDIS
-const setClient = client.set('saved', 'MEAN STACK', function (err, reply) {
-  console.log(reply);
+const SparkPost = require('sparkpost');
+const client = new SparkPost('f110f356cab997d3f2f9a498e83d8dc3cbc34ab1');
+
+async function sendEmail() {
+  try {
+    var responseSend = await client.transmissions.send({
+      content: {
+        from: 'optimus@rudimk.net',
+        subject: 'Congrats Data is changed in REDIS',
+        html: '<html><body><p>Sending mail when data is added in REDIS!</p></body></html>'
+      },
+      recipients: [
+        { address: 'harleen.kaur+11@indiqus.com' }
+      ]
+    })
+    return responseSend;
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+subscriber.on("message", function (channel, message) {
+  console.log("Message '" + message + "' on channel '" + channel + "' arrived!");
+  sendEmail().then(function (emailData) {
+    console.log('emailData', emailData);
+  })
 });
 
-//creates a gets the value from REDIS based on key
-const getClient = client.get('saved', function (err, reply) {
-  console.log(reply);
-});
-
-// listen for connect events
-client.on('connect', function () {
-  console.log('Connected to Redis');
-});
-
-// exports setClient and getClient methods
-exports.setClient = setClient;
-exports.getClient = getClient;
+exports.subs = subscriber.subscribe("email");
+exports.publisher = publisher.publish("email", "send email for the first time");
+//publisher.publish("email", "send email for the second time");
